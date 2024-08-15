dd599f30ae44e72d86647ee8c61a0f8b7faa4300
André Celso <85963049+AndreCelso@users.noreply.github.com>
2024-08-15 07:52:49 -0300

    Commit Inicial Repositório KubeJS para Neo Forge 1.21 15/08/2024 - 07:52

---
+++ server_scripts/All Recipe Removals.js
@@ -0,0 +1,21 @@
+ServerEvents.recipes(Receita => 
+    {
+        Receita.remove({id: "mekanism:processing/gold/dust/from_raw_ore"})
+        Receita.remove({id: "mekanism:processing/iron/dust/from_raw_ore"})
+        Receita.remove({id: "mekanism:processing/osmium/dust/from_raw_ore"})
+        Receita.remove({id: "mekanism:processing/tin/dust/from_raw_ore"})
+        Receita.remove({id: "mekanism:processing/uranium/dust/from_raw_ore"})
+        Receita.remove({id: "mekanism:processing/lead/dust/from_raw_ore"})
+        Receita.remove({id: "mekanism:processing/copper/dust/from_raw_ore"})
+        Receita.remove({id: "allthemodium:mek_processing/allthemodium/dust/from_raw_ore"})
+        Receita.remove({id: "allthemodium:mek_processing/vibranium/dust/from_raw_ore"})
+        Receita.remove({id: "allthemodium:mek_processing/unobtainium/dust/from_raw_ore"})
+        Receita.remove({output: "minecraft:polished_diorite", id: "mekanism:enriching/conversion/diorite/to_polished",})
+        Receita.remove({id: "alltheores:mek_processing/aluminum/dust/from_raw_ore"})
+        Receita.remove({id: "alltheores:mek_processing/nickel/dust/from_raw_ore",})
+        Receita.remove({id: "alltheores:mek_processing/platinum/dust/from_raw_ore",})
+        Receita.remove({id: "alltheores:mek_processing/silver/dust/from_raw_ore",})
+        Receita.remove({id: "alltheores:mek_processing/iridium/dust/from_raw_ore",})
+        Receita.remove({id: "alltheores:mek_processing/zinc/dust/from_raw_ore",})
+    
+    })
