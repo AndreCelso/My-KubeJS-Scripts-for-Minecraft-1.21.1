3456717ab393a9785b78caba34f14624a04224de
André Celso <85963049+AndreCelso@users.noreply.github.com>
2024-08-28 21:46:19 -0300

    Conserta Receita de Slime Block na Reaction Chamber - 28/08/2024 - 21:46

--- server_scripts/Mekanism Scripts/Mekansim Reaction/Reaction.js
+++ server_scripts/Mekanism Scripts/Mekansim Reaction/Reaction.js
@@ -1,6 +1,6 @@
 ServerEvents.recipes(Receita =>
 {
-  function Reaction_Recipes(duration, energyRequired, fluid_Amount, fluid_Input, gas_Amount, gas_Input, gas_Output, gas_Output_Amount, Input_Count, Item_Input, Output_Count, Item_Output)
+  function Reaction_Recipes(duration, fluid_Amount, fluid_Input, gas_Amount, gas_Input, gas_Output, gas_Output_Amount, Input_Count, Item_Input, Output_Count, Item_Output)
   {
     Receita.custom(
     {
@@ -35,6 +35,6 @@ ServerEvents.recipes(Receita =>
    }
 
 //Receita de Slime Block com Cactus na Pressurized Reaction Chamber
-Reaction_Recipes(27, 1000, "minecraft:water", 250, "mekanism:hydrogen", "mekanism:ethene", 50 , 10, "minecraft:cactus", 5, "minecraft:slime_block", "Custom_Slime_Block_PRC")
+Reaction_Recipes(27, 1000, "minecraft:water", 250, "mekanism:hydrogen", "mekanism:ethene", 50 , 10, "minecraft:cactus", 5, "minecraft:slime_block")
 
 })
